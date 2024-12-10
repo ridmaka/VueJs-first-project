58e18c74328d4ae96ec74faa8280a1a38bb07529
ridmaka <ridmakarasanjana12@gmail.com>
2024-12-10 23:39:44 +0530

    again updated

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <h2>Full Name - {{fullName}}</h2>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components: {},
+    data(){
+      return {
+        firstName: "Will",
+        lastName: "Nice",
+      }
+    },
+    computed : {
+      fullName(){
+        return `${this.firstName} ${this.lastName}`
+      }
+    }
+  }
+</script>
+
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
